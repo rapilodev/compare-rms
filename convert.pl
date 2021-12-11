fddd632a16c2c8270fa262d3458c097a3189859d
Milan <mc@radiopiloten.de>
2021-12-11 01:04:11 +0100

    convert.pl: shorten and adjust shift duration from plugin name

--- convert.pl
+++ convert.pl
@@ -3,58 +3,40 @@
 use warnings;
 use strict;
 
-sub error {
-	print "ERROR: " . $_[0] . "\n";
-	exit 1;
-}
+#convert <audio>.1
+# to <audio>.2 using loudnorm
+# to <audio>.3 using <filter>
+# and compare
+
+my $ffmpeg= "$ENV{HOME}/ffmpeg/bin/ffmpeg";
+
+my $ladspa_plugin = 'ebur128-leveler-6s';
+my $label = $ladspa_plugin =~ s/-/_/gr;
+$ladspa_plugin .= ":$label";
+#^ ebur128-leveler-6s:ebur128_leveler_6s
+
+my ($d) = $ladspa_plugin =~ /([\d\.])+s/;
+my $dh = $d / 2.0;
+#^ 6s / 2 = 3
 
 sub execute {
-	my $command = shift;
-	print STDERR "execute: $command\n";
-	system($command);
-	my $exitCode = $? >> 8;
-	if ($exitCode != 0){
-		error("$command died with error $exitCode: $!");
-		exit 1;
-	};
+    my $command = shift;
+    print STDERR "execute: $command\n";
+    system($command);
+    my $exitCode = $? >> 8;
+    die("ERROR: $command died with error $exitCode: $!") if $exitCode;
 }
 
 my $input = $ARGV[0] || '';
-if ( $input eq '' ) {
-	print STDERR "missing file\n";
-	exit 1;
-}
+exit print STDERR "missing file\n" if $input eq '';
+execute "perl loudnorm.pl --file $input.1" unless -e "$input.2";
 
-my $content = qq{
-set("log.file", false)
-set("log.stdout", true)
-input = "$input.1"
-output = "$input.3"
-source = once(single(input))
-source = ladspa.rms_leveler_6s(source)
-clock.assign_new(sync=false,[source])
-output.file( 
-    \%wav(stereo=true, channels=2, samplesize=24, header=true), 
-    output, fallible=true, on_stop=shutdown,source
-)
-};
-
-open my $file, '>convert.liq';
-print $file $content;
-close $file;
-
-#my $command="cd ~/radio/build/rms-leveler/rms-leveler-0.01.0043; make && sudo cp rmsL*.so /usr/lib/ladspa/";
-#execute($command);
-
-$command = "liquidsoap convert.liq";
-execute($command);
-
-$command = "sox $input.3 $input.trim.wav trim 3";
-execute($command);
-
-$command = "mv $input.trim.wav $input.3";
-execute($command);
-
-$command = "perl compareRms.pl -i $input -r 0.1 --no-delete";
-execute($command);
-
+execute "cd $ENV{HOME}/radio/build/rms-leveler/rms-leveler/; make && sudo cp *.so /usr/lib/ladspa/";
+execute "LADSPA_PATH=/usr/lib/ladspa $ffmpeg -y -i $input.1 -af ladspa=$ladspa_plugin -acodec pcm_s24le -ar 44100 $input.rms.wav";
+execute "sox $input.rms.wav $input.trim.wav trim ".$dh; # shift half buffer duration left
+execute "sox $input.trim.wav $input.pad.wav pad 0 ".$dh;
+execute "cp $input.pad.wav $input.3";
+unlink "$input.rms.wav";
+unlink "$input.pad.wav";
+unlink "$input.trim.wav";
+execute "perl compare-rms.pl -i $input -r 0.3";
